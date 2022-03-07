f63b8cf2d1ed5f0279f4dd93046a0aceaba86ca4
Xavier Fernandes <xfdes28@hotmail.com>
2022-03-07 23:01:33 +1300

    Vnet configuration nww

--- vnet.tf
+++ vnet.tf
@@ -11,7 +11,7 @@ resource "azurerm_virtual_network" "example" {
   resource_group_name = azurerm_resource_group.example2.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
-}
+
   subnet {
     name           = "subnet1"
     address_prefix = "10.0.1.0/24"
@@ -25,4 +25,5 @@ resource "azurerm_virtual_network" "example" {
 
   tags = {
     environment = "Production"
+  }
   }

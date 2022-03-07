0079a1d9763dbbb13e43538293ec155ed1a7e7d8
Xavier Fernandes <xfdes28@hotmail.com>
2022-03-07 17:20:08 +1300

    test

--- main.tf
+++ main.tf
@@ -4,16 +4,18 @@ terraform {
       source = "hashicorp/azurerm"
       version = "2.98.0"
 
-      subscriptio_id = var.subscription_id
-      client_id =var.client_id
-      client_secret = var.client_secret
-       tenant_id =var.tenant_id
+      
     }
   }
 }
 
 provider "azurerm" {
-  features {}
+  
+   subscriptio_id = var.subscription_id
+      client_id =var.client_id
+      client_secret = var.client_secret
+       tenant_id =var.tenant_id
+       features {}
 }
 
 resource "azurerm_resource_group" "example" {

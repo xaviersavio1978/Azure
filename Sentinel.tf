e9fa535f60142b69c9bcccffcbe4c91562a90f7e
Xavier Fernandes <xfdes28@hotmail.com>
2022-03-08 15:01:26 +1300

    Adding Sentinel to my module

--- Sentinel.tf
+++ Sentinel.tf
@@ -1,8 +1,5 @@
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
+
 
 resource "azurerm_log_analytics_workspace" "example" {
   name                = "example-workspace"

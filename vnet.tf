6ac1ced7d53f71eb3e31c8f0bbd80bc4b3131ec9
Xavier Fernandes <xfdes28@hotmail.com>
2022-03-11 00:15:22 +1300

    vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,29 +1,22 @@
 
-resource "azurerm_network_security_group" "example" {
-  name                = "example-security-group"
-  location            = azurerm_resource_group.example2.location
-  resource_group_name = azurerm_resource_group.example2.name
-}
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  location            = azurerm_resource_group.example2.location
-  resource_group_name = azurerm_resource_group.example2.name
+module "vnet" {
+  source              = "Azure/vnet/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  vnet_name = "prodvnet"
   address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["vcaresubnet", "payglobalsubnet", "greenteasubnet"]
 
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.example.id
+  subnet_service_endpoints = {
+    vcaresubnet = ["Microsoft.Storage", "Microsoft.Sql"],
+    payglobalsubnet = ["Microsoft.AzureActiveDirectory"]
   }
 
   tags = {
-    environment = "Production"
+    environment = "prod"
+    costcenter  = "it"
   }
-  }
+
+  depends_on = [azurerm_resource_group.example]
+}
